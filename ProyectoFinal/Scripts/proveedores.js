c864c6c36b50e46f6b6210bcc372c9032288f440
yamilaamolina <yamilamolina.utn@gmail.com>
2019-01-28 12:18:15 -0300

    Agrego nuevo formato de vista para proveedores

--- ProyectoFinal/Scripts/proveedores.js
+++ ProyectoFinal/Scripts/proveedores.js
@@ -1,19 +1,48 @@
-﻿//eliminar proveedor
-$(".glyphicon-trash").click(function () {
+﻿//agregar proveedor
+$("#agregarProveedor").click(function () {
+    $("input").val("");
+});
+
+//editar o eliminar proveedor
+$('.delbtn').on('click', function () {
+    var $row = jQuery(this).closest('tr');
+    var $columns = $row.find('td');
+
+    $columns.addClass('row-highlight');
+    var values = "";
+
+    jQuery.each($columns, function (i, item) {
+        switch(i) {
+            case 0:
+                $("#razonSocial").val(item.innerHTML);
+                break;
+            case 1:
+                $("#cuit").val(item.innerHTML);
+                break;
+            case 2:
+                $("#telefono").val(item.innerHTML);
+                break;
+            case 3:
+                $("#email").val(item.innerHTML);
+                break;
+            case 4:
+                $("#direccion").val(item.innerHTML);
+                break;
+        }
+    });
+});
+
+//eliminar proveedor
+$('#eliminarProv').on('click', function () {
 
 });
 
-//editar proveedor
-//$('.delbtn').on('click', function () {
-//    var $row = jQuery(this).closest('tr');
-//    var $columns = $row.find('td');
+//confirmar proveedor
+$('#confirmarProv').on('click', function () {
 
-//    $columns.addClass('row-highlight');
-//    var values = "";
+});
 
-//    jQuery.each($columns, function (i, item) {
-//        values = item.innerHTML;
-//        alert(values);
-//    });
-//    console.log(values);
-//});
+//cancelar proveedor
+$('#cancelarProv').on('click', function () {
+
+});
